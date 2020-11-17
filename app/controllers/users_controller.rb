f862836b7a5f7601bd65b4028b76b121610bf509
Nathan Kane <kaneitalia.rana@gmail.com>
2020-11-17 15:07:00 -0700

    added views, create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  
+  def index
+   @user = User.all
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(users_params)
+
+    if @user.save
+      redirect_to users_path
+    else
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:full_name, :age, :gender, :alive)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+  get 'static_pages/about'
+  # get 'users/index'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/index'
+  # get 'users/remove'
+  # get 'users/show'
+  # get 'users/new'
+  # get 'users/edit'
+  resources :users
+  root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
